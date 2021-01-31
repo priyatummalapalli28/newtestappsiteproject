cb75af1449cf7fa0008d2c4092fdef433f3ddbe5
Venkata <Venkata@tadikonda>
2021-01-30 20:52:03 -0600

    new

--- src/test/java/locators/Test1.java
+++ src/test/java/locators/Test1.java
@@ -2,8 +2,10 @@ package locators;
 
 public class Test1 {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+	public static void main(String[] args) 
+	{
+		//    TODO Auto-generated method stub
+	//jjjj
 
 	}
 
